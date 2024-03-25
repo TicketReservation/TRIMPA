7ab241a0c1f392596317e5494893846d0777af16
RamySassi <rami9b3@live.fr>
2024-03-25 16:17:14 +0100

    first version

---
+++ client/src/index.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+
+const App = () => {
+ 
+
+
+
+  useEffect(() => {
+  }, [])
+
+  return (
+    <div>
+      
+    </div> 
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/client/src");
+var DIST_DIR = path.join(__dirname, "/client/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
