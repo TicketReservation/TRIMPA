01e4b351f2164b8048557b3bf13e05ea096371f6
RamySassi <rami9b3@live.fr>
2024-03-26 22:31:36 +0100

    new

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+// import Footer from './compoents/Footer';
+import Information from './compoents/Information';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello</h1>
+    < Information/>
+     {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
--- client/src/compoents/Footer.js
+++ client/src/compoents/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./../css/Footer.css";
+import style from "../css/footer.css";
 
 
 function Footer() {
