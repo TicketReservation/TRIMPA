82dfcf52b2d44b6c5b09387907fa091644cb0c2c
mahdi <gatrimahdi5@gmail.com>
2024-03-29 06:12:19 +0100

    table , diagram and table price

--- server/Routers/user.routes.js
+++ server/Routers/user.routes.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-const express = require("express")
-const router = express.Router()
-const {getAll,register,deleteOne}=require('../controller/UserController')
-
-router.get("/",getAll)
-router.post("/register",register)
-router.delete("/del/:id",deleteOne)
-=======
 const express = require("express");
 const router = express.Router();
 const {getAll,register,deleteOne,login,UpdateOne}=require('../controller/UserController')
@@ -17,7 +8,6 @@ router.get("/",getAll);
 router.post("/register",register);
 router.post("/login",login)
 router.delete("/del/:id",deleteOne);
->>>>>>> 6a2ea6a97dc321d1d9b7d1b63f9c39cad319c241
 
 
 module.exports = router
